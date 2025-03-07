9a92d94ce34fbd04b3bbb0651e85c6620ab3d8db
imxigh66 <vladakasapcuk@gmail.com>
2025-03-07 13:53:21 +0200

    CQRS-Article

---
+++ Application/Articles/Queries/GetArticlesById.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Articles.Queries
+{
+    internal class GetArticlesById
+    {
+    }
+}
